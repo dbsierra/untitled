fd5cb62399f4fc34f8c4556cf33d3fae93bf1bf9
dbsierra <dsierra4444@gmail.com>
2015-12-17 17:03:44 -0800

    Now with notes instead of frequency

--- index.js
+++ index.js
@@ -5,23 +5,22 @@
 
 var pi2 = Math.PI * 2;
 
-var notes = [65, 77, 65, 98, 103, 130 ];
-var notes2 = [65, 98, 77, 65, 103, 98, 65, 98, 130 ];
+
+var notes2 = ["c3", "g3", "d#3", "c3", "g#3", "g3", "c3", "g3", "c4" ];
+var notesOn = [1,0,1,1,1,1,1,1];
 
 var spd = 9;
 
 export function dsp(t) {
   
-  var stp1 = Math.floor(t*spd)%1;
-  var stp2 = Math.floor(t*spd)%9;
+  var stp2 = Math.floor(t*spd)%8;
   
   var lfo = Math.min(1, Math.sin(t*205)/2+.8);
   
-  var f = notes[stp1];
-  var f2 = notes2[stp2] * 2;
-  var f3 = notes[stp1] * 4;
+  var f2 = notes2[stp2];
+
   
-  var s1 = saw(t, f);
+  var s1 = saw(t, Math.floor(note("c2")));
   
   var unisons = [];
   var unisons_length = 2;
@@ -37,20 +36,18 @@ export function dsp(t) {
     if( i%2 === 0)
       dir = -1
     
-    sU1 += saw(t, f3 + ( Math.floor(i/2))*dir  ) * 1/unisons_length;
-    sU2 += saw(t, f2 + ( Math.floor(i/2))*dir ) * 1/unisons_length;
-    sU3 += saw(t, f + ( Math.floor(i/2))*dir ) * 1/unisons_length;
+    sU1 += saw(t, Math.floor(note("c2")) + ( Math.floor(i/2))*dir  ) * 1/unisons_length * notesOn[stp2];
+    sU2 += saw(t, note(f2) + ( Math.floor(i/2))*dir ) * 1/unisons_length * notesOn[stp2];
+    sU3 += saw(t, Math.floor(note("c2")) + ( Math.floor(i/2))*dir ) * 1/unisons_length ;
     i = i + 1;
   }
   
   var env = Math.max(0, 0.9 - ( (t)%(1/spd) * 15) / (( (t)%(1/spd) * 15) + 1));
 
-  var sF = (s1*.2 +sU3*.7 + sU1 * .8 + sU2*1.3 + sin(f,t)*.9) * env * lfo; 
+  var sF = (s1*.2 +sU3*.7 + sU1 * .8 + sU2*1.3 + sin(note("c2"),t)*.9) * env ; 
   
   return sF;
 }
-
-
 
 
 function sin(t, f){
@@ -60,3 +57,29 @@ function sin(t, f){
 function saw(t, f){
   return t*f - Math.round(t*f);
 }
+
+
+
+
+
+
+//Note to frequency - taken from https://github.com/opendsp/note
+function note(n){
+  if ('string' === typeof n) n = stringToNote(n);
+  return Math.pow(2, (n - 57)/12) * 440;
+}
+
+function stringToNote(s){
+  s = s.split('');
+  var octave = parseInt(s[s.length - 1], 10);
+  if (isNaN(octave)) octave = 4;
+  var note = s[0].toLowerCase();
+  var flat = s[1] === 'b';
+  var sharp = s[1] === '#';
+  var notes = 'ccddeffggaab';
+  return notes.indexOf(note) + (octave * 12) + sharp - flat;
+}
+
+
+
+
